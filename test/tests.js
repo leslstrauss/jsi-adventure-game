4e5ce2af7a2d17917d2753071d975a5fc9296972
Kali Kiger <kalikiger@gmail.com>
2014-09-11 10:07:25 -0700

    added good failing test, started work on function for the game

--- test/tests.js
+++ test/tests.js
@@ -1,34 +1,54 @@
 var chai = require('chai');
 var expect = chai.expect;
-var game = require('./the-game');
+var game = require('../the-game');
+var entranceLocator = game.entranceLocator;
 
 describe("the-game()", function() {
   // given an object, it looks for the entrance
-  it("looks for the entrance given an object", function() {
-  //reads the rooms file and expects entrance to equal south
-  var map = //object
-  var rooms = map.rooms;
-  var roomB = rooms[1];
-  var entrance = roomB.entrance;
-  expect(entranceLocator(someMap)).to.eql("south")
+  it.skip("looks for the entrance given an object", function() {
+    //reads the rooms file and expects entrance to equal south
+    var roomB = {
+      "name": "B",
+      "north": "E",
+      "east": "C",
+      "south": null,
+      "west": "A",
+      "entrance": "south"
+    };
+    var roomC = {
+      "name": "C",
+      "north": null,
+      "east": null,
+      "south": null,
+      "west": "B"
+    };
+    var map = { "rooms": [roomB, roomC] };
+    var entrance = entranceLocator(map);
+    expect(entrance).to.eql(roomB, 'Entrance room should be B');
   });
 });
 
-given a object/file, look for the entrance
+// given a object/file, look for the entrance
 
-it introduces the game
+// it introduces the game
 
-it states what room you are in
+// it states what room you are in
 
-given a room, which directions can you go
-it offers available directions
+// given a room, which directions can you go
+// it offers available directions
 
-it repeats when you choose a direction that is not available
+// describe('options', function(){
+//   var room? = {
 
-it receives user input
+//   }
+// });
 
-it moves to the next room
+// it repeats when you choose a direction that is not available
 
-it says you found the treasure when you reach K
+// it receives user input
 
-it tells you how to quit
+// it moves to the next room
+
+// it says you found the treasure when you reach K
+
+// it tells you how to quit
--- the-game.js
+++ the-game.js
@@ -1,8 +1,10 @@
-require('jsi-gamelib');
+var gameLibrary = require('jsi-gamelib');
 
-You are now playing The Best Game Ever. When prompted, please use -n to select north,
-When you reach the correct room, the game will announce that you have found the secret treasure
-and prompt you to press -q to quit.
-You are now in room C. Would you like to go East or North?
+// You are now playing The Best Game Ever. When prompted, please use -n to select north,
+// When you reach the correct room, the game will announce that you have found the secret treasure
+// and prompt you to press -q to quit.
+// You are now in room C. Would you like to go East or North?
 
-entranceLocator()
+module.exports.entranceLocator = function () {
+  
+};
