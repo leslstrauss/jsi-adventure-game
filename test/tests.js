c3433c70e81d42d305648a0abe9eb43c3e2ce20c
Kali Kiger <kalikiger@gmail.com>
2014-09-11 10:36:15 -0700

    Wrote documentation for entranceLocator.

--- test/tests.js
+++ test/tests.js
@@ -52,3 +52,8 @@ describe("the-game()", function() {
 // it says you found the treasure when you reach K
 
 // it tells you how to quit
+
+// You are now playing The Best Game Ever. When prompted, please use -n to select north,
+// When you reach the correct room, the game will announce that you have found the secret treasure
+// and prompt you to press -q to quit.
+// You are now in room C. Would you like to go East or North?
